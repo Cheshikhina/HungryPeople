954f1b72d11434caac9c41f13db438de6258c10b
Daria <d.sumina@mail.ru>
2020-06-18 00:55:07 +0300

    Добавляет проект на гитхаб

---
+++ build/js/main.js
@@ -0,0 +1,154 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./source/js/main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./source/js/main.js":
+/*!***************************!*\
+  !*** ./source/js/main.js ***!
+  \***************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modules_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/common */ "./source/js/modules/common.js");
+// import 'svgxuse';
+// import 'lazysizes';
+// import './modules/sliders';
+// import './forIE/polyfills';
+ // import scroll from './modules/scroll';
+// import menu from './modules/menu';
+// import modal from './modules/modal';
+// import form from './modules/form';
+
+window.addEventListener('DOMContentLoaded', function () {
+  Object(_modules_common__WEBPACK_IMPORTED_MODULE_0__["default"])(); // scroll();
+  // menu('.header__btn');
+  // modal('[data-modal="open_popup"]', '#popup');
+  // modal('.trigger', '#popup');
+  // modal('.page_slider');
+  // if (document.querySelectorAll('form').length) {
+  //   document.querySelectorAll('form').forEach((item) => {
+  //     form(item);
+  //   });
+  // }
+  // document.addEventListener('click', function (evt) {
+  //   console.log(evt.target);
+  // })
+});
+
+/***/ }),
+
+/***/ "./source/js/modules/common.js":
+/*!*************************************!*\
+  !*** ./source/js/modules/common.js ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var common = function common() {
+  var noFocus = function noFocus() {
+    document.addEventListener('mouseup', function (evt) {
+      if (evt.target.tagName !== 'A' && evt.target.tagName !== 'BUTTON') {
+        return;
+      }
+
+      evt.target.blur();
+    });
+  };
+
+  noFocus();
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (common);
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=main.js.map
---
+++ source/js/main.js
@@ -0,0 +1,27 @@
+// import 'svgxuse';
+// import 'lazysizes';
+// import './modules/sliders';
+// import './forIE/polyfills';
+import common from './modules/common';
+// import scroll from './modules/scroll';
+// import menu from './modules/menu';
+// import modal from './modules/modal';
+// import form from './modules/form';
+
+window.addEventListener('DOMContentLoaded', () => {
+  common();
+  // scroll();
+  // menu('.header__btn');
+  // modal('[data-modal="open_popup"]', '#popup');
+  // modal('.trigger', '#popup');
+  // modal('.page_slider');
+
+  // if (document.querySelectorAll('form').length) {
+  //   document.querySelectorAll('form').forEach((item) => {
+  //     form(item);
+  //   });
+  // }
+  // document.addEventListener('click', function (evt) {
+  //   console.log(evt.target);
+  // })
+});
